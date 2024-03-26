2e3a0bcce4d534528e646f28b5b089ff2e5a0326
Enghav <128684916+Enghav@users.noreply.github.com>
2024-03-26 13:50:57 +0700

    route

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import Home from './view/Home.jsx';
+import About from './view/About.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// eslint-disable-next-line no-undef
+reportWebVitals();
